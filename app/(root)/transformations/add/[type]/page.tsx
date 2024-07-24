8ba843ee9c07971db4c46473813743b6ec5a71b4
chhantea <r.lalnghenchhana@gmail.com>
2024-07-24 18:52:23 +0530

    wrap it for today-1

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import Header from '@/components/share/header'
+import React, { use } from 'react';
+import {transformationTypes} from '@/constants'
+import TransformationForm from '@/components/share/TransformationForm';
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-const AddTransformationType = () => {
+const AddTransformationType = async ({params:{type}}: SearchParamProps) => {
+  const {userId} = auth();
+  const transformation = transformationTypes[type];
+  if(!userId) redirect('/sign-in');
+  // const user = await getUserById(userId); 
+  const user ={
+    _id : "abc",
+    creditBalance:10
+  }
   return (
-    <div>
-      AddTransformationType
-    </div>
+    <>
+      <Header
+       title={transformation.title}
+       subtitle={transformation.subTitle}
+      />
+      <TransformationForm
+       action='Add'
+       userId={user._id}
+       type={transformation.type as TransformationTypeKey}
+       creditBalance={user.creditBalance}
+      />
+    </>
   )
 }
 
