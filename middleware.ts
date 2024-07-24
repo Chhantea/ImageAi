0706a0ac230071ddcfabea8d59a27ea841c66cad
chhantea <r.lalnghenchhana@gmail.com>
2024-07-24 15:49:17 +0530

    chk log data

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
-import { authMiddleware, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware} from "@clerk/nextjs/server";
 
 
-const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+// const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
 
-export default clerkMiddleware((auth, request) => {
-  if(isPublicRoute(request)) {
-    auth().protect();
-  }
-});
-// export default authMiddleware({
-//   publicRoutes: [ '/','/api/webhooks/clerk', '/api/webhooks/stripe']
+// export default clerkMiddleware((auth, request) => {
+//   if(isPublicRoute(request)) {
+//     auth().protect();
+//   }
 // });
+export default authMiddleware({
+  publicRoutes: [ '/','/api/webhooks/clerk', '/api/webhooks/stripe'],
+});
 // export default clerkMiddleware();
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
