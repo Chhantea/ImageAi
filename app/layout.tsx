bfdb28dfc7e33f17c5f8e9e531788e00c84f8eb9
chhantea <r.lalnghenchhana@gmail.com>
2024-07-30 13:30:28 +0530

    add pwa and top loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,51 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import NextTopLoader from 'nextjs-toploader'; // for toploader on page load
 
 const IBMPlex = IBM_Plex_Sans({
    subsets: ["latin"],
    weight:["400","500",'600','700'],
    variable:'--font-ibm-plex'
    });
-
+   const APP_NAME = "ImageAi App";
+   const APP_DEFAULT_TITLE = "Image ai playground.";
+   const APP_TITLE_TEMPLATE = "%s - ImageAi App";
+   const APP_DESCRIPTION = "Best ImageAi app in the world!";
 export const metadata: Metadata = {
-  title: "ImageAi",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
   description: "Image ai playground.",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +60,20 @@ export default function RootLayout({
       variables:{colorPrimary:'#6C47FF'}
     }}>
     <html lang="en">
-      <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>{children}</body>
+      <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>
+      <NextTopLoader
+        color="#5f49ff"
+        initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={true}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
+        {children}
+        </body>
     </html>
     </ClerkProvider>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: ''
-        }
-      ]
-    }
-  };
+// const nextConfig = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'res.cloudinary.com',
+//           port: ''
+//         }
+//       ]
+//     }
+//   };
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: ''
+      }
+    ]
+  }
+});
+
